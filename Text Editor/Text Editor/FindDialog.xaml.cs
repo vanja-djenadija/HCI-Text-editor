bca8c43f77487f370dbb80b7977cc0ae4ea21d10
vanja-djenadija <130909026+vanja-djenadija@users.noreply.github.com>
2023-04-17 16:29:06 +0200

    Initial commit

---
+++ Text Editor/Text Editor/FindDialog.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Text_Editor
+{
+    /// <summary>
+    /// Interaction logic for FindDialog.xaml
+    /// </summary>
+    public partial class FindDialog : Window
+    {
+        private RichTextBox rtbEditor;
+        List<TextRange> ranges = new();
+        static List<String> words = new();
+        MatchCollection matchCollection = null;
+        string previousString = "";
+        int count = 0;
+
+        public FindDialog(RichTextBox rtbEditor)
+        {
+            this.rtbEditor = rtbEditor;
+            InitializeComponent();
+            foreach (string word in words)
+            {
+                findComboBox.Items.Add(word);
+                findComboBox.SelectedItem = previousString;
+            }
+        }
+
+        private void Count(object sender, RoutedEventArgs e)
+        {
+            string text = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd).Text;
+            MatchCollection matches = Regex.Matches(text, findComboBox.Text);
+            MessageBox.Show("Found " + matches.Count + " occurences.");
+        }
+
+        private void FindNext(object sender, RoutedEventArgs e)
+        {
+            if (findComboBox.Text.Trim().Equals(previousString))
+            {
+                count++;
+            }
+            else
+            {
+                ranges.Clear();
+                count = 0;
+                string newWord = findComboBox.Text.Trim();
+                if (String.IsNullOrEmpty(newWord))
+                    return;
+                if (!words.Contains(newWord))
+                    words.Add(newWord);
+                Regex reg = new Regex(newWord, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                TextPointer position = rtbEditor.Document.ContentStart;
+
+                while (position != null)
+                {
+                    if (position.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text)
+                    {
+                        string text = position.GetTextInRun(LogicalDirection.Forward);
+                        matchCollection = reg.Matches(text);
+
+                        foreach (Match match in matchCollection)
+                        {
+                            TextPointer start = position.GetPositionAtOffset(match.Index);
+                            TextPointer end = start.GetPositionAtOffset(newWord.Length);
+
+                            TextRange textrange = new TextRange(start, end);
+                            ranges.Add(textrange);
+                        }
+                    }
+                    position = position.GetNextContextPosition(LogicalDirection.Forward);
+                }
+            }
+            rtbEditor.SelectAll();
+            rtbEditor.Selection.ApplyPropertyValue(TextElement.BackgroundProperty, new SolidColorBrush(Colors.White));
+
+            previousString = findComboBox.Text.Trim();
+
+            if (matchCollection == null)
+                return;
+            if (count < matchCollection.Count)
+            {
+                TextRange range = ranges.ElementAt(count);
+                range.ApplyPropertyValue(TextElement.BackgroundProperty, new SolidColorBrush(Colors.Yellow));
+            }
+            else
+            {
+                previousString = "";
+            }
+
+
+            // upadate all searched words in combobox
+            findComboBox.Items.Clear();
+            foreach (string word in words)
+            {
+                findComboBox.Items.Add(word);
+                findComboBox.SelectedItem = previousString;
+            }
+        }
+
+        private void FindAll(object sender, RoutedEventArgs e)
+        {
+            rtbEditor.SelectAll();
+            rtbEditor.Selection.ApplyPropertyValue(TextElement.BackgroundProperty, new SolidColorBrush(Colors.White));
+
+            Regex reg = new Regex(findComboBox.Text.Trim(), RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            TextPointer position = rtbEditor.Document.ContentStart;
+            List<TextRange> ranges = new List<TextRange>();
+            while (position != null)
+            {
+                if (position.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text)
+                {
+                    string text = position.GetTextInRun(LogicalDirection.Forward);
+                    var matchs = reg.Matches(text);
+
+                    foreach (Match match in matchs)
+                    {
+                        TextPointer start = position.GetPositionAtOffset(match.Index);
+                        TextPointer end = start.GetPositionAtOffset(findComboBox.Text.Trim().Length);
+
+
+                        TextRange textrange = new TextRange(start, end);
+                        ranges.Add(textrange);
+                    }
+                }
+                position = position.GetNextContextPosition(LogicalDirection.Forward);
+            }
+
+            foreach (TextRange range in ranges)
+            {
+                range.ApplyPropertyValue(TextElement.BackgroundProperty, new SolidColorBrush(Colors.Yellow));
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            rtbEditor.SelectAll();
+            rtbEditor.Selection.ApplyPropertyValue(TextElement.BackgroundProperty, new SolidColorBrush(Colors.White));
+            MainWindow.findWindowOpen = false;
+        }
+    }
+}
---
+++ Text Editor/Text Editor/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.ComponentModel;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows.Media;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Text_Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string? fileName;
+        public static bool findWindowOpen = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+        }
+
+        private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbFontFamily.SelectedItem != null)
+                rtbEditor.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+        }
+
+        private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (findWindowOpen)
+                return;
+            rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+        }
+
+        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
+
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            cmbFontFamily.SelectedItem = temp;
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
+            cmbFontSize.Text = temp.ToString();
+            // update status bar
+            TextPointer tp1 = rtbEditor.Selection.Start.GetLineStartPosition(0);
+            TextPointer tp2 = rtbEditor.Selection.Start;
+
+            int column = tp1.GetOffsetToPosition(tp2);
+
+            int someBigNumber = int.MaxValue;
+            int lineMoved, currentLineNumber;
+            rtbEditor.Selection.Start.GetLineStartPosition(-someBigNumber, out lineMoved);
+            currentLineNumber = -lineMoved;
+
+            lblCursorPosition.Text = "Line: " + currentLineNumber.ToString() + " Column: " + column.ToString();
+
+        }
+
+        private void Open(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                fileName = dlg.FileName;
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+                TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                range.Load(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void SaveAs(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                fileName = dlg.FileName;
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
+                TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void Save(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(fileName))
+                SaveAs(sender, e);
+            else
+            {
+                TextRange range;
+                FileStream fStream;
+                range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                fStream = new FileStream(fileName, FileMode.Create);
+                range.Save(fStream, DataFormats.Rtf);
+                fStream.Close();
+                MessageBox.Show("File is saved.");
+            }
+        }
+
+        private void Exit(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (MessageBox.Show("Do you want to save changes?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                Save(sender, e);
+            }
+            Application.Current.Shutdown();
+        }
+
+        private void New(object sender, ExecutedRoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+
+        private void NewWindow(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+        }
+
+        private void Find(object sender, RoutedEventArgs e)
+        {
+            FindDialog findDialog = new(rtbEditor);
+            findDialog.Show();
+            findWindowOpen = true;
+        }
+    }
+}
+
